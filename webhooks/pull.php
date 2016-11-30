9fea40b9afaa3c34ea6343008f228079ab12f4c6
antares.devs <antares.devs@gmail.com>
2016-11-30 09:07:13 -0500

    webhook

--- webhooks/pull.php
+++ webhooks/pull.php
@@ -1,4 +1,11 @@
 <?php
+echo '<pre>';
 var_dump(shell_exec('git stash 2>&1'));
+
+
 var_dump(shell_exec('git pull 2>&1'));
 
+chdir('../');
+var_dump(shell_exec('mkdocs build 2>&1'));
+
+echo '</pre>';
