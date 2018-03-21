6425ffb346dd53e54caf34fe04df9919fffdc659
mountstone <mountstone@users.noreply.github.com>
2018-03-21 09:42:13 +0100

    Update pull.php

--- webhooks/pull.php
+++ webhooks/pull.php
@@ -1,11 +1,8 @@
 <?php
 echo '<pre>';
 var_dump(shell_exec('git stash 2>&1'));
-
-
 var_dump(shell_exec('git pull 2>&1'));
-
+var_dump(shell_exec('php codehilite.php 2>&1'));
 chdir('../');
-var_dump(shell_exec('mkdocs build 2>&1'));
-
+var_dump(shell_exec('mkdocs build -c 2>&1'));
 echo '</pre>';
